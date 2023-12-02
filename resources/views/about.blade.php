07a34c71b22f3c4ac2eae3dd9ffd9b79059e34f5
Yashraj Kabra <yashrajkabra@gmail.com>
2023-12-02 20:43:05 +0530

    Updated About Section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,7 +11,7 @@
  <link rel="stylesheet" href="{{ asset('styles3.css') }}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> About Section</title>
  </head>
  <body> 
         <div class="about-content">
