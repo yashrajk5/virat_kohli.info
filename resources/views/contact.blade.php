1f9784b4433d01858e9ea9eb7bfcad9cbc69c383
Yashraj Kabra <yashrajkabra@gmail.com>
2023-12-02 20:46:34 +0530

    Updated History & Contact Section

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,7 +10,7 @@
             <link rel="stylesheet" href="{{ asset('styles6.css') }}">
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
+            <title>Contact sections</title>
             
         </body>
         </html>
--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -10,7 +10,7 @@
             <link rel="stylesheet" href="{{ asset('styles4.css') }}">
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
+            <title>History Section</title>
         </head>
         <body>
         
